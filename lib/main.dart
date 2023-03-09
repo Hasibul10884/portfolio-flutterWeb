f771935df5fadc82f5d2fa98c9506d5e1e1e93b1
Hasibul10884 <hasibulhasan193@gmail.com>
2023-03-09 06:49:18 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Portfolio Website',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: portfolio(),
+    );
+  }
+}
+class portfolio extends StatelessWidget {
+  const portfolio({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.black,
+      ),
+    ) ;
+  }
+}
+
+
